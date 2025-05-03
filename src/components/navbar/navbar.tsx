5572de4825e9fe7339ab8a78cdaf08fc069ab330
Rajat Satashiya <rajat09satashiya@gmail.com>
2025-05-03 16:33:11 +0530

    initial commit

---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Link from "next/link";
+
+function Navbar() {
+  return (
+    <div className="flex justify-between p-5 px-12 items-center">
+      <div className="w-40">
+        <img
+          src="https://www.onlygfx.com/wp-content/uploads/2018/09/6-feather-drawing-2.png"
+          alt="Footprint logo"
+        />
+      </div>
+      <ul className="flex gap-10">
+        <li>Home</li>
+        <li>About</li>
+        <li>Pricing</li>
+        <li>Contact</li>
+      </ul>
+      <Link href="/login">
+        <div>Login</div>
+      </Link>
+    </div>
+  );
+}
+
+export default Navbar;
